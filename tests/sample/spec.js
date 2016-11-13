6e8c69656c1053703e3225a09997918c78129d78
Simon McManus <mcmanus.simon@gmail.com>
2016-11-13 10:49:35 +0000

    adding nightwatch tests

---
+++ tests/sample/spec.js
@@ -0,0 +1,12 @@
+module.exports = {
+  '/index.html': {
+    page: 'home'
+  },
+  '/contact.html': {
+    page: 'contact'
+  },
+  options: {
+    outputDir: '/docs',
+    files: ['./client/router-compiled.js']
+  }
+}
