052bab8d7b381c57b64ee1f06651bbc0330c445e
Simon McManus <mcmanus.simon@gmail.com>
2016-11-27 17:25:12 +0000

    use full apth

--- tests/e2e/spec.js
+++ tests/e2e/spec.js
@@ -1,7 +1,7 @@
 module.exports = {
   'Page should be added to the container': function (browser) {
     browser
-      .url('http://localhost:5004/')
+      .url('http://localhost:5004/index.html')
       .waitForElementVisible('body', 1000)
       .assert.containsText('#container', 'home')
       .url('http://localhost:5004/contact.html')
