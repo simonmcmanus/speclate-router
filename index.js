f8b6a9f803f99236dce7f70e80f4e0ecae317355
Simon McManus <mcmanus.simon@gmail.com>
2017-09-27 08:21:50 +0100

    load first class - experinment

--- index.js
+++ index.js
@@ -11,9 +11,25 @@ module.exports = function (routerOptions, speclateOptions) {
   var $container = $(speclateOptions.container || '#container')
   var loadingClass = routerOptions.loadingClass || 'loading'
   var el = document.querySelector('html')
-  el.classList.add(loadingClass)
+
 
   page('*', function (context, next) {
+
+    var render = el.getAttribute('data-speclate-render')
+
+    // page is not being loaded from cache so just show the rendered page for speed.
+
+
+    console.log(context.init, render, context.init && render !== 'layout-first')
+    // Its the first page load and its deffo not from cache so just use the html already on the page.
+    if (context.init && render === null) { // !== 'layout-first'
+      console.log('very first page load')
+      return
+    }
+    el.classList.add(loadingClass)
+
+    console.log('not first page load')
+
     el.classList.add(loadingClass)
 
     routerOptions.preFetch && routerOptions.preFetch($container)
@@ -23,8 +39,6 @@ module.exports = function (routerOptions, speclateOptions) {
       routeName = '/index'
     }
     var specPath = '/api/speclate' + routeName + '.json'
-
-
 
     el.setAttribute('data-speclate-url', context.pathname)
 
