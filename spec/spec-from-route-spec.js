7636550b739feb25057ab247e455e6e9728404a8
Simon McManus <mcmanus.simon@gmail.com>
2017-11-23 10:21:11 +0000

    npm run test runs unit tests

--- spec/spec-from-route-spec.js
+++ spec/spec-from-route-spec.js
@@ -2,31 +2,43 @@
 var specFromRoute = require('../lib/spec-from-route')
 
 describe('SpecFromRoute', function () {
+  describe('Given /', () => {
+    var pathName = '/'
+    var expectedOut = '/api/speclate/index.json'
+    var specPath = specFromRoute(pathName)
+
+    it('should result in ' + expectedOut, function () {
+      expect(specPath).toEqual(expectedOut)
+    })
+  })
+
+  describe('Given /', () => {
+    var pathName = ''
+    var expectedOut = '/api/speclate/index.json'
+    var specPath = specFromRoute(pathName)
+
+    it('should result in ' + expectedOut, function () {
+      expect(specPath).toEqual(expectedOut)
+    })
+  })
 
   describe('Given a full  url string', () => {
-
-
     var pathName = '/bacon.html'
     var expectedOut = '/api/speclate/bacon.json'
     var specPath = specFromRoute(pathName)
 
-     it('should result in ' + expectedOut, function () {
-
+    it('should result in ' + expectedOut, function () {
       expect(specPath).toEqual(expectedOut)
-     })
+    })
   })
 
+  describe('Given a a url ending with a forward slash (folder containing index.html)', () => {
+    var pathName = '/bacon/'
+    var expectedOut = '/api/speclate/bacon/index.json'
+    var specPath = specFromRoute(pathName)
 
-
-  describe('Given a a url ending with a forward slash', () => {
-
-      var pathName = '/bacon/'
-      var expectedOut = '/api/speclate/bacon/index.json'
-      var specPath = specFromRoute(pathName)
-
-        it('should result in ' + expectedOut, function () {
-
-        expect(specPath).toEqual(expectedOut)
-        })
+    it('should result in ' + expectedOut, function () {
+      expect(specPath).toEqual(expectedOut)
     })
+  })
 })
