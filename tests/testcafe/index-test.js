3fb337e1b3189ceefaf5481fb29ad003f5b73693
Simon McManus <mcmanus.simon@gmail.com>
2018-03-06 08:43:23 +0000

    checks the container

--- tests/testcafe/index-test.js
+++ tests/testcafe/index-test.js
@@ -1,8 +1,11 @@
 
+import { Selector } from 'testcafe'
+
 fixture`Getting Started`
   .page`http://localhost:5004`
 
 test('My first test', async t => {
   await t
     .click('.contact')
+    .expect(Selector('#container')).contains('Home page');
 })
