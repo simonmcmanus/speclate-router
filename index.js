906b76f097363cc2ea652d20166ee11f50be3624
Simon McManus <mcmanus.simon@gmail.com>
2019-05-10 14:14:44 +0100

    pass selectors round - bit messy

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var FetchPage = require('./lib/fetch-page')
 var SpecFromRoute = require('./lib/spec-from-route')
 var requests = []
 
-var onClick = function (elements, routerOptions, speclateOptions) {
+var onClick = function (selectors, elements, routerOptions, speclateOptions) {
   return function (e) {
     const link = e.currentTarget
     const newLocation = link.getAttribute('href')
@@ -14,12 +14,12 @@ var onClick = function (elements, routerOptions, speclateOptions) {
       var state = {}
       var stateName = ''
       window.history.pushState(state, stateName, newLocation)
-      pageChange(newLocation, elements, routerOptions, speclateOptions)
+      pageChange(selectors, newLocation, elements, routerOptions, speclateOptions)
     }
   }
 }
 
-var pageChange = function (newLocation, elements, routerOptions, speclateOptions) {
+var pageChange = function (selectors, newLocation, elements, routerOptions, speclateOptions) {
   var loadingClass = routerOptions.loadingClass || 'loading'
   elements.html.classList.add(loadingClass)
   routerOptions.preFetch && routerOptions.preFetch(elements.container)
@@ -34,11 +34,6 @@ var pageChange = function (newLocation, elements, routerOptions, speclateOptions
   }
 
   // check the spec here to see what strategy it should be using.
-  // todo - get this some other wat
-  const selectors = {
-    html: 'html',
-    container: '#container'
-  }
   requests.push(new FetchPage(specPath, elements, selectors, loadingClass, routerOptions))
 }
 
@@ -57,7 +52,7 @@ var setupLinks = function (routerOptions, speclateOptions) {
   var links = document.getElementsByTagName('a')
   for (var i = 0; i < links.length; i++) {
     // TODO:  could check here if the link is listed in the spec
-    links[i].addEventListener('click', onClick(elements, routerOptions, speclateOptions), { capture: false })
+    links[i].addEventListener('click', onClick(selectors, elements, routerOptions, speclateOptions), { capture: false })
   }
 }
 
